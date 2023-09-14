a2396439be36300c0d275ea3e7b70a404d13db73
Hudson Bruno <hb.natal@gmail.com>
2023-09-14 18:44:02 -0300

    feat: added check when entering or leaving voice channel

--- src/check/cog.py
+++ src/check/cog.py
@@ -1,11 +1,13 @@
 import logging
+from datetime import datetime
 from typing import Optional
 
-from discord import Interaction, app_commands
+from discord import Interaction, Member, VoiceState, app_commands
 from discord.ext import commands
 
+from check.entities.check import Check
 from check.repositories.in_memory_check_repository import InMemoryCheckRepository
-from check.use_cases.check import CheckUseCase
+from check.use_cases.check import CheckUseCase, CheckUseCaseDTO
 from check.validators.jsonschema.jsonschema_check_validator import (
     JsonSchemaCheckValidator,
 )
@@ -34,6 +36,23 @@ class CheckCog(commands.Cog):
             f"{check.user.mention} checked at {check.date.strftime('%d/%m/%y %H:%M')}!",
             ephemeral=True,
         )
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ):
+        entered = before.channel is None and after.channel is not None
+        left = before.channel is not None and after.channel is None
+
+        if entered or left:
+            use_case = CheckUseCase(self.repository)
+            check = await use_case.execute(
+                CheckUseCaseDTO(member, datetime.now(), Check.InsertType.AUTO)
+            )
+            await check.user.send(
+                "Auto checked by voice chat at"
+                f" {check.date.strftime('%d/%m/%y %H:%M')}!"
+            )
 
     async def cog_app_command_error(self, interaction, error):
         if isinstance(error, app_commands.CommandInvokeError):
